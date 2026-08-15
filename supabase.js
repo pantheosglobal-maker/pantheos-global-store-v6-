fc0d844537a9c29b0c3702e515017f5fc539182a
pantheosglobal-maker <pantheosglobal@gmail.com>
2026-08-16 01:29:04 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,7 +2,7 @@ const SUPABASE_URL =
 "https://gwlyvkbyrdjzrobrrdun.supabase.co";
 
 const SUPABASE_KEY =
-"PASTE_YOUR_ANON_KEY_HERE";
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd3bHl2a2J5cmRqenJvYnJyZHVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY4MDk1MTUsImV4cCI6MjEwMjM4NTUxNX0.Rku6-9M5GDc3mG2Cb6zZJmUh-_rJd4O96kcXsUI1nyI";
 
 const supabase =
 window.supabase.createClient(
