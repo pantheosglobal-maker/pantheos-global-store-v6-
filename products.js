5acf5347a00e8830b50e6602363186cfd471a8bb
pantheosglobal-maker <pantheosglobal@gmail.com>
2026-08-16 01:20:35 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,1 +1,22 @@
+let selectedGame = null;
+let selectedCategory = null;
+let selectedPackage = null;
 
+async function loadProducts(){
+
+const { data, error } =
+await supabase
+.from("products")
+.select("*");
+
+if(error){
+console.log(error);
+return;
+}
+
+console.log("Products Loaded");
+console.log(data);
+
+}
+
+loadProducts();
